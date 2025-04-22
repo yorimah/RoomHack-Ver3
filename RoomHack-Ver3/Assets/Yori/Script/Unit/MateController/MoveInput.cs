e192361d0ead40d9d80a1c9adc38d52263ccf3dc
yorima <yoribaseball623@gmail.com>
2025-04-22 09:43:38 +0900

    スプライト追加、スクリプト追加

--- RoomHack-Ver3/Assets/Yori/Script/Unit/MateController/MoveInput.cs
+++ RoomHack-Ver3/Assets/Yori/Script/Unit/MateController/MoveInput.cs
@@ -1,43 +1,38 @@
-//using UnityEngine;
-//using UnityEngine.InputSystem;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
-//public class MoveInput
-//{
-//    private GameInputs _gameInputs;
+public class MoveInput
+{
+    private GameInputs _gameInputs;
 
-//    private Vector2 _moveInputValue;
+    private Vector2 _moveInputValue;
 
+    public void Init()
+    {
+        _gameInputs = new GameInputs();
 
-//    // 1なら攻撃入力、0なら待機入力
-//    private int onAttack;
+        // Actionイベント登録
+        _gameInputs.Player.Move.started += OnMove;
+        _gameInputs.Player.Move.performed += OnMove;
+        _gameInputs.Player.Move.canceled += OnMove;
+        _gameInputs.Enable();
+    }
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        // Moveアクションの入力取得
+        _moveInputValue = context.ReadValue<Vector2>();
+    }
 
-//    public void Init()
-//    {
-//        onAttack = 0;
-//        _gameInputs = new GameInputs();
+    public Vector2 MoveValue()
+    {
+        return _moveInputValue;
+    }
 
-//        // Actionイベント登録
-//        _gameInputs.Player.Move.started += OnMove;
-//        _gameInputs.Player.Move.performed += OnMove;
-//        _gameInputs.Player.Move.canceled += OnMove;
-//        _gameInputs.Enable();
-//    }
-//    private void OnMove(InputAction.CallbackContext context)
-//    {
-//        // Moveアクションの入力取得
-//        _moveInputValue = context.ReadValue<Vector2>();
-//    }
+    public void OnDestroy()
+    {
+        // 自身でインスタンス化したActionクラスはIDisposableを実装しているので、
+        // 必ずDisposeする必要がある
+        _gameInputs?.Dispose();
+    }
 
-//    public Vector2 MoveValue()
-//    {
-//        return _moveInputValue;
-//    }
-
-//    public void OnDestroy()
-//    {
-//        // 自身でインスタンス化したActionクラスはIDisposableを実装しているので、
-//        // 必ずDisposeする必要がある
-//        _gameInputs?.Dispose();
-//    }
-
-//}
+}
