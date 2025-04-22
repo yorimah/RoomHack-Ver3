e42b3f4df196dc1c38760c81f71c70bf0604af18
yorima <yoribaseball623@gmail.com>
2025-04-22 12:11:39 +0900

    移動実装

--- RoomHack-Ver3/Assets/Yori/Script/Unit/MateController/TestMove.cs
+++ RoomHack-Ver3/Assets/Yori/Script/Unit/MateController/TestMove.cs
@@ -2,7 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestMove 
+public class TestMove :MonoBehaviour
 {
+    private MoveInput moveInput ;
 
+    private Vector2 moveVector;
+
+    private Rigidbody2D playerRigidbody2D;
+
+    private float MOVESPEED = 10;
+    public void Start()
+    {
+        moveInput = new MoveInput();
+
+        moveInput.Init();
+
+        playerRigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        playerRigidbody2D.velocity = PlayerMoveVector(moveInput.MoveValue()) * MOVESPEED;
+    }
+
+    public Vector2 PlayerMoveVector(Vector2 inputMoveVector)
+    {
+        moveVector = inputMoveVector;
+        return moveVector;
+    }
 }
