5d6e83eccf21e3806a05ed079d04d99beb8cf490
kokoA7V <oktakeru@icloud.com>
2025-06-09 14:00:58 +0900

    EliteEnemyおためしせいさく

---
+++ RoomHack-Ver3/Assets/Koko/Scripts/EliteGuard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EliteGuard : SecurityGuard
+{
+    private void Start()
+    {
+        Debug.Log("warota");
+    }
+
+}
