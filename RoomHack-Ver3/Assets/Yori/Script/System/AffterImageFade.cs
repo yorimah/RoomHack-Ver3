cb3b38f8a50d4dc40e6ac89321f92d4223bd3b48
yorima <yoribaseball623@gmail.com>
2025-07-09 10:41:01 +0900

    残像完成

---
+++ RoomHack-Ver3/Assets/Yori/Script/System/AffterImageFade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AffterImageFade : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Color fadeColor = new Color(0, 0, 0, 0.8f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spriteRenderer.color -= fadeColor * GameTimer.Instance.ScaledDeltaTime;
+        if (spriteRenderer.color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- RoomHack-Ver3/Assets/Yori/Script/System/AfterImageEffecter.cs
+++ RoomHack-Ver3/Assets/Yori/Script/System/AfterImageEffecter.cs
@@ -1,23 +1,21 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class AfterImageEffecter : MonoBehaviour
 {
     private Sprite affterImageSprite;
     private SpriteRenderer affterImageSpriteRender;
     private Color initColor;
-   
+
 
     private float timer = 0;
     private float generateEffectTime = 0.2f;
 
     private GameObject affterImageObject;
-    private List<GameObject> affterImageObjects;
 
 
     void Start()
     {
-        affterImageObjects = new List<GameObject>();
         affterImageSprite = this.GetComponent<SpriteRenderer>().sprite;
         initColor = this.GetComponent<SpriteRenderer>().color;
         initColor.a = 0.5f;
@@ -30,15 +28,20 @@ public class AfterImageEffecter : MonoBehaviour
             timer += GameTimer.Instance.ScaledDeltaTime;
             if (timer >= generateEffectTime)
             {
-                affterImageObject = new GameObject(gameObject.name + "AfferImage");
-                affterImageObject.transform.position = this.transform.position;
-                affterImageObject.transform.rotation = this.transform.rotation;
-                affterImageSpriteRender = affterImageObject.AddComponent<SpriteRenderer>();
-                affterImageSpriteRender.sprite = affterImageSprite;
-                affterImageSpriteRender.color = initColor;
-                affterImageObjects.Add(affterImageObject);
-                timer = 0;
+                GenerateAffterImage();
             }
         }
+    }
+
+    void GenerateAffterImage()
+    {
+        affterImageObject = new GameObject(gameObject.name + "AfferImage");
+        affterImageObject.transform.position = this.transform.position;
+        affterImageObject.transform.rotation = this.transform.rotation;
+        affterImageSpriteRender = affterImageObject.AddComponent<SpriteRenderer>();
+        affterImageObject.AddComponent<AffterImageFade>();
+        affterImageSpriteRender.sprite = affterImageSprite;
+        affterImageSpriteRender.color = initColor;
+        timer = 0;
     }
 }
