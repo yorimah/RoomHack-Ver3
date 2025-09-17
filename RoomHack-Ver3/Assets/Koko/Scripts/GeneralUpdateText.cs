7c3f80321a024fd3e137afa1f1ffdb23d092444a
kokoA7V <oktakeru@icloud.com>
2025-09-17 14:16:17 +0900

    Player仮UI制作中

--- RoomHack-Ver3/Assets/Koko/Scripts/GeneralUpdateText.cs
+++ RoomHack-Ver3/Assets/Koko/Scripts/GeneralUpdateText.cs
@@ -10,9 +10,9 @@ public class GeneralUpdateText : MonoBehaviour
     [SerializeField, Tooltip("テキスト開始スイッチ")]
     public bool isRunning = false;
 
-    // アクティブ状態になると動作開始
-    //[SerializeField, Tooltip("アクティブ時に起動するかどうか")]
-    //bool activeStart = true;
+    //アクティブ状態になると動作開始
+    [SerializeField, Tooltip("アクティブ時に起動するかどうか")]
+    bool activeStart = true;
 
     int timer = 0;
 
@@ -31,32 +31,31 @@ public class GeneralUpdateText : MonoBehaviour
     
     void Update()
     {
-        //if (activeStart)
-        //{
-        //    isRunning = this.gameObject.activeInHierarchy;
-        //}
+        if (this.gameObject.activeInHierarchy && !isRunning)
+        {
+            isRunning = true;
+            textIndex = 0;
+        }
 
         // 起動中
         if (isRunning)
         {
-            // ディレイ0なら即座に全文出る
-            if (delay == 0)
-            {
-                textIndex = inputText.Length;
-                dispText.text = inputText.Substring(0, textIndex);
-            }
-
-            // 
+            // テキスト送り
             if (textIndex < inputText.Length)
             {
                 if (timer >= delay)
                 {
                     textIndex++;
-                    dispText.text = inputText.Substring(0, textIndex);
                     timer = 0;
                 }
 
                 timer++;
+            }
+
+            // ディレイ0なら即座に全文出る
+            if (delay == 0)
+            {
+                textIndex = inputText.Length;
             }
 
         }
@@ -64,5 +63,8 @@ public class GeneralUpdateText : MonoBehaviour
         {
             textIndex = 0;
         }
+
+        // テキスト表示
+        dispText.text = inputText.Substring(0, textIndex);
     }
 }
---
+++ RoomHack-Ver3/Assets/Koko/Scripts/PlayerUITest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUITest : MonoBehaviour
+{
+    [SerializeField]
+    float maxHp = 1;
+
+    [SerializeField]
+    float nowHp = 1;
+
+    [SerializeField]
+    GameObject healthBar;
+
+    [SerializeField]
+    GameObject player;
+
+    private void Start()
+    {
+        maxHp = UnitCore.Instance.MAXHP;
+
+    }
+
+    private void Update()
+    {
+        this.transform.position = player.transform.position;
+
+        nowHp = UnitCore.Instance.NowHP;
+
+        Vector3 scale = healthBar.gameObject.transform.localScale;
+        scale.x = nowHp / maxHp;
+        healthBar.gameObject.transform.localScale = scale;
+
+    }
+}
