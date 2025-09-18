6d4ae91a8082e643758f9e72b82b2ecec2c278b2
kokoA7V <oktakeru@icloud.com>
2025-09-18 16:43:13 +0900

    Tool管理プログラム作成中

---
+++ RoomHack-Ver3/Assets/Koko/Scripts/ToolData/ToolDataBank.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName ="ToolData")]
+
+public class ToolDataBank : ScriptableObject
+{
+    public List<ToolData> toolDataList = new List<ToolData>();
+}
+
+//System.Serializableを設定しないと、データを保持できない(シリアライズできない)ので注意
+[System.Serializable]
+public class ToolData
+{
+    public string toolName;
+    public int toolCost;
+    public string toolText;
+    public Sprite toolSprite;
+}
+
+enum tool
+{
+    none,
+    CCTVHack,
+    Bind,
+    Blind,
+    OverHeat,
+    Detonation
+}
---
+++ RoomHack-Ver3/Assets/Koko/Scripts/ToolDeskUIDisp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolDeskUIDisp : MonoBehaviour
+{
+    [SerializeField]
+    ToolDataBank toolDataBank;
+
+    int toolCost;
+
+    private void Start()
+    {
+        Debug.Log(toolDataBank.toolDataList[0].toolName);
+        Debug.Log(toolDataBank.toolDataList[0].toolCost);
+        Debug.Log(toolDataBank.toolDataList[0].toolText);
+    }
+}
