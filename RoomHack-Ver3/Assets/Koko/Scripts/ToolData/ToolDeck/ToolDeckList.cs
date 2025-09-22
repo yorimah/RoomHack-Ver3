61c4ca6c65b3625aa4f803376c454485f88decc8
kokoA7V <oktakeru@icloud.com>
2025-09-22 14:49:10 +0900

    デッキシステム基盤完成

---
+++ RoomHack-Ver3/Assets/Koko/Scripts/ToolData/ToolDeck/ToolDeckList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "ToolDeckList")]
+
+public class ToolDeckList : ScriptableObject
+{
+    public List<tool> deckList = new List<tool>();
+}
+
+////System.Serializableを設定しないと、データを保持できない(シリアライズできない)ので注意
+//[System.Serializable]
+//public class ToolData
+//{
+//    public string toolName;
+//    public int toolCost;
+//    public string toolText;
+//    public Sprite toolSprite;
+//    public GameObject toolEvent;
+//}
