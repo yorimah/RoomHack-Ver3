5c5709900dd2d87c00a0dee62c3b8fa2e92da7de
kokoA7V <oktakeru@icloud.com>
2025-10-20 15:19:11 +0900

    ２どろーおかあさん、あの日のように

--- RoomHack-Ver3/Assets/Koko/Scripts/ToolData/ToolEvent/Resource/ToolEvent_LightResource.cs
+++ RoomHack-Ver3/Assets/Koko/Scripts/ToolData/ToolEvent/Resource/ToolEvent_LightResource.cs
@@ -2,7 +2,7 @@
 
 public class ToolEvent_LightResource : ToolEvent
 {
-    public override toolTag thisToolTag { get; set; } = toolTag.none;
+    public override toolTag thisToolTag { get; set; } = toolTag.LightResource;
 
     protected override void Enter()
     {
@@ -11,7 +11,7 @@ public class ToolEvent_LightResource : ToolEvent
 
     protected override void Execute()
     {
-
+        ToolManager.Instance.ToolDraw(2);
         EventEnd();
     }
 
