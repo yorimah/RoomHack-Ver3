12fa0dfd37e9a2ddc0bddcc45897008c7845e00f
kokoA7V <oktakeru@icloud.com>
2025-11-11 10:31:07 +0900

    カメラエフェクト関連の作業中

---
+++ RoomHack-Ver3/Assets/Koko/Scripts/Camera/CanvasCameraChanger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CanvasCameraChanger : MonoBehaviour
+{
+    [SerializeField, Header("要アタッチ")]
+    Camera actionCamera;
+
+    [SerializeField, Header("要アタッチ")]
+    Camera hackCamera;
+
+    Canvas hackCanvas;
+
+    private void Start()
+    {
+        hackCanvas = GetComponent<Canvas>();
+    }
+
+    private void Update()
+    {
+        if (GameTimer.Instance.IsHackTime)
+        {
+            hackCanvas.worldCamera = hackCamera;
+
+        }
+        else
+        {
+            hackCanvas.worldCamera = actionCamera;
+        }
+    }
+
+}
