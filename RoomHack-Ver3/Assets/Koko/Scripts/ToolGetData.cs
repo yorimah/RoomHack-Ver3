a0a71c35c429e8309c3a501f5b2681439620236f
kokoA7V <oktakeru@icloud.com>
2025-11-13 12:34:54 +0900

    ToolGetDataList完成、ToolGetManagerに突っ込むとランダムに選んでくれるぜベイビー

--- RoomHack-Ver3/Assets/Koko/Scripts/ToolGetData.cs
+++ RoomHack-Ver3/Assets/Koko/Scripts/ToolGetData.cs
@@ -1,20 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+
 [CreateAssetMenu(menuName = "ToolGetDataList")]
 public class ToolGetDataList : ScriptableObject
 {
-    public List<ToolGetData> upGradeCardList = new List<ToolGetData>();
+    public List<ToolGetData> dataList = new List<ToolGetData>();
 }
 
-public class ToolGetData : MonoBehaviour
+[System.Serializable]
+public class ToolGetData
 {
-
-
-
-    [System.Serializable]
-    public class UpGradeData
-    {
-        public ToolTag toolTag;
-        public int toolWeight;
-    }
+    public ToolTag toolTag;
+    public int toolWeight;
 }
