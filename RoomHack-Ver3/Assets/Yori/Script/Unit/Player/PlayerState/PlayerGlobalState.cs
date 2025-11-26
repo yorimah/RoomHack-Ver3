2809318bb0ba5bad4f9acf18cd97f2411d485f2d
yorima <yoribaseball623@gmail.com>
2025-11-26 12:10:36 +0900

    使ってない変数を削除

--- RoomHack-Ver3/Assets/Yori/Script/Unit/Player/PlayerState/PlayerGlobalState.cs
+++ RoomHack-Ver3/Assets/Yori/Script/Unit/Player/PlayerState/PlayerGlobalState.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 public class PlayerGlobalState : IPlayerState
 {
     private PlayerShot playerShot;
-    private Rigidbody2D rigidbody2D;
     public PlayerGlobalState(IGetGunData gunData, Material material, IPlayerInput playerInput,  
         GameObject bulletPre, GameObject player,IHaveGun haveGun,Rigidbody2D _rigidbody2D)
     {
         playerShot = new PlayerShot(gunData, material, player, bulletPre, playerInput,haveGun);
-        rigidbody2D = _rigidbody2D;
     }
 
     public void Enter()
