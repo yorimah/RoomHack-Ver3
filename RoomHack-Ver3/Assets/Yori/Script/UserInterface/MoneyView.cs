2f3e111bd900fffc26c08fbdf379f7c4c34ee8c7
デスクトップよりま <yoribaseball623@gmail.com>
2026-01-08 15:03:48 +0900

    敵に装甲を追加

---
+++ RoomHack-Ver3/Assets/Yori/Script/UserInterface/MoneyView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+public class MoneyView : MonoBehaviour
+{
+    [Inject]
+    IGetMoneyNum getMoneyNum;
+
+    private int money = 0;
+
+    [SerializeField, Header("残金を表示するテキスト")]
+    private Text moneyText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moneyText = moneyText.GetComponent<Text>();
+        moneyText.text = "Money : " + money;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (money != getMoneyNum.GetMoneyNum())
+        {
+            money = getMoneyNum.GetMoneyNum();
+            moneyText.text = "Money : " + money;
+        }
+    }
+}
