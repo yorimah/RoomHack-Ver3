7b2267b7da86a9e51b5caf0a52f908a166459b78
デスクトップよりま <yoribaseball623@gmail.com>
2026-01-11 15:01:07 +0900

    フォルダ整理、シャットダウン機能追加中

---
+++ RoomHack-Ver3/Assets/Yori/Script/UserInterface/ShutDownWindowSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+public class ShutDownWindowSystem : WindowSystem
+{
+    Vector2 shutDownWindowSize = new Vector2(500, 500);
+    public override async UniTask PopUpWindow()
+    {
+        windowRect.sizeDelta += new Vector2(0, 10);
+        while (windowRect.rect.width < shutDownWindowSize.x)
+        {
+            windowRect.sizeDelta += new Vector2(shutDownWindowSize.x, 0);
+            await UniTask.WaitForSeconds(waitSeconds);
+        }
+        while (windowRect.rect.height <shutDownWindowSize.y)
+        {
+            windowRect.sizeDelta += new Vector2(0, shutDownWindowSize.y);
+            await UniTask.WaitForSeconds(waitSeconds);
+        }
+        windowRect.sizeDelta = new Vector2(Screen.width / 2, shutDownWindowSize.y);
+        dragCollider.enabled = true;
+        buttonObj.SetActive(true);
+    }
+}
