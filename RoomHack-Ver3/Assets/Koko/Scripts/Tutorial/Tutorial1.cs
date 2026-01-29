3e9bdf24dc351a9312487a00cc5c4ab90c58370d
kokoA7V <oktakeru@icloud.com>
2026-01-30 00:39:25 +0900

    チュートリアル作成中

---
+++ RoomHack-Ver3/Assets/Koko/Scripts/Tutorial/Tutorial1.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Tutorial1 : MonoBehaviour
+{
+    [SerializeField, Header("あたっち")]
+    TutorialManager tutorialManager;
+
+    [SerializeField]
+    List<TutorialData> tutorialList = new List<TutorialData>();
+
+    int index = 0;
+    bool isExplain = false;
+
+    float timer = 0;
+
+    TutorialData nullData;
+
+    private void Awake()
+    {
+        nullData.muskPos = Vector2.zero;
+        nullData.muskSize = new Vector2(1920, 1080);
+        nullData.textPos = Vector2.zero;
+        nullData.explains = " ";
+    }
+
+    private void Update()
+    {
+        // 説明オンオフ
+        if (isExplain)
+        {
+            tutorialManager.SetStatus(tutorialList[index]);
+        }
+        else
+        {
+            tutorialManager.SetStatus(nullData);
+        }
+
+        switch (index)
+        {
+            case 0:
+                // 説明開始条件
+                if (true) isExplain = true;
+
+                // 説明中
+                if (isExplain)
+                {
+
+                }
+
+                // 説明終了条件
+                if (true)
+                {
+                    isExplain = false;
+                    index++;
+                }
+
+                break;
+        }
+    }
+}
+
+public class TutorialData
+{
+    public Vector2 muskPos;
+    public Vector2 muskSize;
+
+    public Vector2 textPos;
+    public string explains;
+
+    public float time;
+}
---
+++ RoomHack-Ver3/Assets/Koko/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField, Header("あたっち")]
+    RectTransform muskRect;
+    [SerializeField]Vector2 muskPos;
+    [SerializeField]Vector2 muskSize;
+
+    [SerializeField, Header("あたっち")]
+    GeneralUpdateText explainText;
+    [SerializeField] Vector2 textPos;
+    [SerializeField] string explains;
+
+    private void Update()
+    {
+        Vector2 moveVec = (muskPos - (Vector2)muskRect.localPosition) / 10;
+        muskRect.localPosition += (Vector3)moveVec;
+
+        Vector2 scaleVec = (muskSize - muskRect.sizeDelta) / 10;
+        muskRect.sizeDelta += scaleVec;
+
+        explainText.GetComponent<RectTransform>().localPosition = textPos;
+    }
+
+    public void SetStatus(TutorialData _data)
+    {
+        muskPos = _data.muskPos;
+        muskSize = _data.muskSize;
+        textPos = _data.textPos;
+        explains = _data.explains;
+    }
+}
